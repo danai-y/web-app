07aed028c76f0e8bc12c1fbb53b326a73963216a
danai-y <danai_yodkaewreung@cmu.ac.th>
2020-11-19 04:15:00 +0700

    setup angularfire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVYLeMQrMzWy9zxKlp5Pn2XeTDGrUuaac",
+    authDomain: "web-app-7ef92.firebaseapp.com",
+    databaseURL: "https://web-app-7ef92.firebaseio.com",
+    projectId: "web-app-7ef92",
+    storageBucket: "web-app-7ef92.appspot.com",
+    messagingSenderId: "798532824502",
+    appId: "1:798532824502:web:59811a2ab9aa0714472517",
+    measurementId: "G-WR6YRBZQM3"
+  }
 };
 
 /*
