6545254b5b2d539d24a765b9156f83fcd6826991
danai-y <danai_yodkaewreung@cmu.ac.th>
2020-11-19 12:19:12 +0700

    add cus-nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CusNavComponent } from './cus-nav/cus-nav.component';
 import { ResNavComponent } from './res-nav/res-nav.component';
 
 const routes: Routes = [
-  { path: '', component: ResNavComponent }
+  { path: '', component: ResNavComponent },
+  { path: 'table', component: CusNavComponent },
 ];
 
 @NgModule({
---
+++ src/app/cus-nav/cus-nav.component.spec.ts
@@ -0,0 +1,40 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { CusNavComponent } from './cus-nav.component';
+
+describe('CusNavComponent', () => {
+  let component: CusNavComponent;
+  let fixture: ComponentFixture<CusNavComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [CusNavComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CusNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
