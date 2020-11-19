6d974b2d7f91ff3ff30285cd3850246a1574400a
danai-y <danai_yodkaewreung@cmu.ac.th>
2020-11-19 20:18:48 +0700

    setup angularfire2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     component: ResNavComponent,
     children: [
       {
-        path: 'table-list',
+        path: '',
         component: TableListComponent
       },
       {
